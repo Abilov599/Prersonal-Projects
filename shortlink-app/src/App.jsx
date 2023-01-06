8ed8f0b1f066bef666cea7633ed93b2d95b0278c
Abilov Jeyhun <abilovv599@gmail.com>
2023-01-07 03:39:26 +0400

    shorter is working

--- shortlink-app/src/App.jsx
+++ shortlink-app/src/App.jsx
@@ -4,7 +4,6 @@ import Header from "./layouts/header";
 import Main from "./layouts/main";
 
 function App() {
-  // const SHORTER_API = `https://api.shrtco.de/v2/shorten?url=${link}`;
   return (
     <div className="App">
       <Header />
--- shortlink-app/src/components/link-shorter-form/index.jsx
+++ shortlink-app/src/components/link-shorter-form/index.jsx
@@ -1,13 +1,52 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./index.scss";
 
 const Shortly = () => {
+  const [userInput, setUserInput] = useState("");
+  const [shortenedLink, setShortenedLink] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await axios(
+        `https://api.shrtco.de/v2/shorten?url=${userInput}`
+      );
+      setShortenedLink(response.data.result.full_short_link);
+    } catch (e) {
+      throw e;
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
   return (
     <div id="shortly">
-      <form action="">
-        <input placeholder="Shorten a link here..." type="text" />
-        <button id="shorten-it" type="submit">Shorten It!</button>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          placeholder="Shorten a link here..."
+          type="text"
+          value={userInput}
+          onChange={(e) => {
+            setUserInput(e.target.value);
+          }}
+        />
+        <button id="shorten-it" type="submit">
+          Shorten It!
+        </button>
       </form>
+      <div className="mt-5">
+        {shortenedLink}
+        <button className="border-2 border-blue-500 text-blue-500 font-medium px-5 py-2 ml-4 rounded-md">
+          Copy URL to Clipboard
+        </button>
+      </div>
     </div>
   );
 };
